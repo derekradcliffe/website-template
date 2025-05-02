6e550ed44a2b4fa5cf4a7df8bf858feb38acb532
Derek Radcliffe <dereksradcliffe@gmail.com>
2025-05-02 11:51:10 -0400

    Aded ghpages

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,15 @@
+// import { createRootRoute } from '@tanstack/react-router';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 // import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 // import Header from '../components/Header'
+// import App from './index';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       {/* <Header /> */}
+      {/* <App /> */}
 
       <Outlet />
       {/* <TanStackRouterDevtools /> */}
